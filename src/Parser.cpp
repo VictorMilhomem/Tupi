4635d7ca85f9170c465343c6d7c68723aa8d8c76
Victor Milhomem <victormllosada@gmail.com>
2024-11-11 17:40:39 -0300

    Simple Parser Created

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,1 +1,72 @@
 #include "Parser.h"
+
+/*
+ * SIMPLE GRAMMAR
+<program> ::= <function>
+<function> ::= "fn" <id> "(" ")" ":" Type "{" <statement> "}"
+<statement> ::= "return" <exp> ";"
+<exp> ::= <unary_op> <exp> | <int>
+<unary_op> ::= "!" | "~" | "-"
+ */
+
+std::unique_ptr<Ast::Constant> Parser::Parser::parseConstant() {
+    auto token = getCurrentToken();
+    std::string value_str = token.text;
+    int value = std::stoi(value_str);
+    return std::make_unique<Ast::Constant>(value);
+}
+
+
+std::unique_ptr<Ast::Unary> Parser::Parser::parseUnary() {
+    auto token_op = getPreviousToken();
+    auto token = getCurrentToken();
+    std::string op = token_op.text;
+    // for now we are parsing only int expr
+    match(token.kind, Lexer::TokenKind::INTEGER);
+    std::unique_ptr<Ast::Expression> expr = parseExpression();
+    return std::make_unique<Ast::Unary>(op, expr);
+
+}
+
+
+std::unique_ptr<Ast::Expression> Parser::Parser::parseExpression() {
+    auto token = getCurrentToken();
+    if (token.kind == Lexer::TokenKind::BITWISE ||
+        token.kind == Lexer::TokenKind::NOT ||
+        token.kind == Lexer::TokenKind::MINUS) {
+        pos++;
+        auto unary = parseUnary();
+        return std::make_unique<Ast::Expression>(unary);
+    }
+    match(token.kind, Lexer::TokenKind::INTEGER);
+    auto integer = parseConstant();
+    return std::make_unique<Ast::Expression>(integer);
+}
+
+std::unique_ptr<Ast::Statement> Parser::Parser::parseStatement() {
+    auto token = getCurrentToken();
+    match(token.kind, Lexer::TokenKind::RETURN);
+    auto expr = parseExpression();
+    match(token.kind, Lexer::TokenKind::SEMICOLON);
+    return std::make_unique<Ast::ReturnStmt>(std::move(expr));
+}
+
+
+std::unique_ptr<Ast::Function> Parser::Parser::parseFunction() {
+    auto token = getNextToken();
+    match(token.kind, Lexer::TokenKind::FN);
+    match(token.kind, Lexer::TokenKind::IDENTIFIER);
+    std::string name = token.text;
+    match(token.kind, Lexer::TokenKind::LPAREN);
+    // after match the arguments
+    match(token.kind, Lexer::TokenKind::RPAREN);
+    match(token.kind, Lexer::TokenKind::COLON);
+    match(token.kind, Lexer::TokenKind::TYPE);
+    match(token.kind, Lexer::TokenKind::LBRACE);
+    // parse the function body
+    auto body = parseStatement();
+    match(token.kind, Lexer::TokenKind::RBRACE);
+    return std::make_unique<Ast::Function>(name, body);
+}
+
+
--- src/Parser.h
+++ src/Parser.h
@@ -1,12 +1,47 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include <vector>
 
+#include "Ast.h"
+#include "Lexer.h"
 
-class Parser {
+namespace Parser {
+    class Parser {
+    private:
+        std::vector<Lexer::Token> tokens;
+        size_t pos;
 
-};
+        std::unique_ptr<Ast::Function> parseFunction();
+        std::unique_ptr<Ast::Statement> parseStatement();
+        std::unique_ptr<Ast::Expression> parseExpression();
+        std::unique_ptr<Ast::Unary> parseUnary();
+        std::unique_ptr<Ast::Constant> parseConstant();
 
+        void match(Lexer::TokenKind token_kind, Lexer::TokenKind expected) {
+            if (token_kind != expected) throw std::runtime_error("Syntax Error: Unexpected token");
+            pos++;
+        };
 
+        Lexer::Token getCurrentToken() {
+            return tokens[pos];
+        }
+
+        Lexer::Token getNextToken() {
+            return tokens[pos++];
+        }
+
+        Lexer::Token getPreviousToken() {
+            return tokens[pos - 1];
+        }
+
+    public:
+        explicit Parser(const std::vector<Lexer::Token>& tokens) : tokens(tokens), pos(0) {}
+
+        std::unique_ptr<Ast::Program> parseProgram() {
+            return std::make_unique<Ast::Program>(parseFunction());
+        }
+    };
+}
 
 #endif //PARSER_H
