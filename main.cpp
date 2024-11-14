b5f5b91560a0d84cadc22b86028f43b235a3e1fc
Victor Milhomem <victormllosada@gmail.com>
2024-11-14 15:58:23 -0300

    Printing the assembly that will be generated

--- main.cpp
+++ main.cpp
@@ -8,17 +8,20 @@ int main() {
     std::string input = "fn main() : int {  return -2; }";
     auto lexer = Lexer::Lexer(input);
     auto tokens = lexer.tokenize();
+    std::cout << "============ Lexer Tokens ============" << std::endl;
     Lexer::Lexer::debug(tokens);
     auto parser = Parser::Parser(tokens);
     auto ast = parser.parseProgram();
     const auto printer = std::make_unique<Ast::PrettyPrinter>();
+    std::cout << "============ Parsing Tree ============" << std::endl;
     printer->printProgram(*ast);
+
     auto codegen = std::make_unique<CodeGen>(std::move(ast));  // Move ownership
-
-    // Generate assembly code
     auto assembly = codegen->convert_program();
-
+    std::cout << "============ Assembly Tree ============" << std::endl;
     // Print the generated assembly
-    print_program(*assembly);
+    print_program(*assembly, 0);
+    std::cout << "============ Code Generated ============" << std::endl;
+    codegen->program_generator();
 
 }
